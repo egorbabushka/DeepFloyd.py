dae3d3f68cbdc3bc557b04d5592eba50452c5c39
egorbabushka <130289288+egorbabushka@users.noreply.github.com>
2023-05-08 18:56:34 +0300

    Update deepfloydasync.py

--- deepfloydasync.py
+++ deepfloydasync.py
@@ -31,15 +31,13 @@ class deepfloydapi:
           case 'send_hash':
             await websocket.send(json.dumps({'fn_index': 20, 'session_hash': self.sessionhash}))
           case 'queue_full':
-            return {
-              'error': 'Service Unavailable'
-            }
+            return 'Service Unavailable'
           case 'send_data':
             await websocket.send(json.dumps({'fn_index': 20, 'data': [prompt, negative, math.floor(random.random() * 100000000), 4, 7, "smart50", 100], 'session_hash': self.sessionhash}))
           case 'process_completed':
             imgurls = [f'https://{self.url}/file={i["name"]}' for i in out["output"]["data"][0]]
             self.temp = out["output"]["data"][2]
-            return {'info': out['output'], 'img_urls': imgurls}
+            return imgurls
   async def upscale(self, index):
     if not self.temp:
       raise ImagesNotGenerated()
@@ -52,9 +50,7 @@ class deepfloydapi:
           case 'send_hash':
             await websocket.send(json.dumps({'fn_index': 34, 'session_hash': self.sessionhash}))
           case 'queue_full':
-            return {
-              'error': 'Service Unavailable'
-            }
+            return 'Service Unavailable'
           case 'send_data':
             await websocket.send(json.dumps({'fn_index': 34, 'data': [self.temp, index, math.floor(random.random() * 100000000), 4, "smart50", 50, self.prompt, self.negative, math.floor(random.random() * 100000000), 9, 40], 'session_hash': self.sessionhash}))
           case 'process_completed':
